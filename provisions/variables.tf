0390e8cc72a8e14d3cb7fe1bb7f9ee5d5bf767ca
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2018-12-04 11:08:12 +0100

    'terraformFiles'

---
+++ provisions/variables.tf
@@ -0,0 +1,15 @@
+variable "heroku_email" {
+    type = "string"
+}
+
+variable "heroku_api_key" {
+    type = "string"
+}
+
+variable "github_organization" {
+    type = "string"
+}
+
+variable "github_api_key" {
+    type = "string"
+}
